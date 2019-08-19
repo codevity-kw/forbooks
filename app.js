48e401bf3cf9a97f3d4827d8689ee0c2ef74ac06
dalal_alsabeeh <dalal_alsabeeh@outlook.com>
2019-08-19 17:47:11 +0300

    about v2

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ require("ejs")
 var express = require("express")
 
 var app = express()
-
 app.set("view engine", "ejs")
-app.get("/about",function(req,res){
-res.render("about")
-}
-)
 
 // Route below -->!
 app.get("/", function(req, res){
     res.render("home")
 })
-
+app.get("/about",function(req,res){
+    res.render("about")
+    }
+    )
 app.listen(3000, function(){
     console.log("Server Start")
 })
