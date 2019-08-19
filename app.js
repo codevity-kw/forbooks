0c3cd87560366cb2681382e9af57180617953019
Muhammad Alkulaib <muhammad.alkulaib@gmail.com>
2019-08-19 17:27:19 +0300

    adding navbar servicehome and app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = express()
 
 app.set("view engine", "ejs")
 
+// Route below -->!
+app.get("/", function(req, res){
+    res.render("home")
+})
+
 app.listen(3000, function(){
     console.log("Server Start")
 })
