d8e4c954ff3dc30f34fb06bc5e9d149f6a027bf5
Hussain <h.alhogal@gmail.com>
2019-08-19 07:48:39 -0700

    team page

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var app = express()
 
 app.set("view engine", "ejs")
 
+
+
+
+
+app.get("/team", function(req,res) {
+    res.render("team")
+})
+
+
 app.listen(3000, function(){
     console.log("Server Start")
 })
