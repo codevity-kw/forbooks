bd0c2709c185902672276d161dca398fcdbfc339
dalal_alsabeeh <dalal_alsabeeh@outlook.com>
2019-08-19 17:32:34 +0300

    about page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require("express")
 var app = express()
 
 app.set("view engine", "ejs")
+app.get("/about",function(req,res){
+res.render("about")
+}
+)
 
 app.listen(3000, function(){
     console.log("Server Start")
